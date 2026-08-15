def6e9e9bcb650e268c1c208c489f12bb840c48d
TiaoFeng <mahirovo@duck.com>
2026-08-15 15:44:25 +0800

    feature: main.rs实现了一个可堪一用的todo list cli入口，可以通过Add List Done Delete控制todo list

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod commands;
+mod storage;
+mod task;
+
+use clap::{Parser, Subcommand};
+use storage::FilePath;
+
+#[derive(Parser)]
+#[command(name = "todo")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { content: String },
+    List,
+    Done { id: usize },
+    Delete { id: usize },
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let path = FilePath::new(None);
+    let result = match cli.command {
+        Commands::Add { content } => commands::add_task(content, &path),
+        Commands::List => {
+            commands::list_task(&path);
+            Ok(())
+        }
+        Commands::Done { id } => commands::complete_task(id, &path),
+        Commands::Delete { id } => commands::delete_task(id, &path),
+    };
+    if let Err(e) = result {
+        eprintln!("Error:{}", e);
+        std::process::exit(1);
+    }
+}
