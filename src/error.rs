c3e0a4310a0443370174e6da51b7324189e2f8b3
TiaoFeng <mahirovo@duck.com>
2026-08-20 19:58:05 +0800

    refactor: 对error.rs新增单元测试

--- src/error.rs
+++ src/error.rs
@@ -82,3 +82,114 @@ impl Error for AppError {
         }
     }
 }
+
+#[cfg(test)]
+mod error_test {
+    use super::*;
+    use std::error::Error;
+
+    fn create_io_error() -> std::io::Error {
+        std::io::Error::new(std::io::ErrorKind::NotFound, "no such file")
+    }
+
+    fn create_serde_error() -> serde_json::Error {
+        serde_json::from_str::<serde_json::Value>("{abcdefg}").unwrap_err()
+    }
+
+    #[test]
+    fn test_display() {
+        let src = create_io_error();
+        let io = AppError::Io {
+            operation: "write to file",
+            path: "test1.json".to_string(),
+            source: src,
+        };
+        assert_eq!(
+            io.to_string(),
+            "failed to write to file 'test1.json': no such file"
+        );
+
+        let corrupted = AppError::Corrupted {
+            path: "test2.json".to_string(),
+            source: create_serde_error(),
+        };
+        assert_eq!(
+            corrupted.to_string(),
+            format!(
+                "task file 'test2.json' is corrupted: {}",
+                corrupted.source().unwrap()
+            )
+        );
+
+        assert_eq!(
+            AppError::InvalidDeadline {
+                input: "ab-cd-ef".to_string()
+            }
+            .to_string(),
+            "Invalid deadline format 'ab-cd-ef', expected {%Y-%m-%d} or {%Y-%m-%dT%H:%M:%S}"
+        );
+
+        assert_eq!(
+            AppError::InvalidLocalTime.to_string(),
+            "Time Conversion Error. It may be due to daylight saving time."
+        );
+
+        assert_eq!(
+            AppError::NothingToChange.to_string(),
+            "Nothing to change, Please enter one or more subcommands"
+        );
+        assert_eq!(
+            AppError::TaskNotFound { no: 1 }.to_string(),
+            "Task not found: no 1, run `list` to check current numbers"
+        );
+    }
+
+    #[test]
+    fn test_source() {
+        assert!(
+            AppError::Corrupted {
+                path: "test1.json".to_string(),
+                source: create_serde_error()
+            }
+            .source()
+            .is_some()
+        );
+        assert!(
+            AppError::Io {
+                operation: "test_op",
+                path: "test2.json".to_string(),
+                source: create_io_error()
+            }
+            .source()
+            .is_some()
+        );
+        assert!(
+            AppError::InvalidDeadline {
+                input: "input1".to_string()
+            }
+            .source()
+            .is_none()
+        );
+        assert!(AppError::InvalidLocalTime.source().is_none());
+        assert!(AppError::NothingToChange.source().is_none());
+        assert!(AppError::TaskNotFound { no: 1 }.source().is_none());
+    }
+
+    #[test]
+    fn pub_trait() {
+        let err = create_io_error();
+        let io_err = io_err("test_op", "test1.json".to_string(), err);
+        match io_err {
+            AppError::Io {
+                operation,
+                path,
+                source,
+            } => {
+                assert_eq!(operation, "test_op");
+                assert_eq!(path, "test1.json".to_string());
+                assert_eq!(source.to_string(), create_io_error().to_string());
+            }
+            _ => unreachable!(),
+        }
+    }
+}
